5d3a6f729a71578bc9bc3e2bf8afe8376b1df355
Joasia00 <Asiawegiel123@wp.pl>
2022-04-03 15:27:56 +0200

    Powinno być git

--- src/main/java/pl/sda/maven/Main.java
+++ src/main/java/pl/sda/maven/Main.java
@@ -1,4 +1,7 @@
 package pl.sda.maven;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gotowe!");
+    }
 }
--- src/main/java/pl/sda/maven/Pracawzespole.java
+++
@@ -1,4 +0,0 @@
-package pl.sda.maven;
-
-public class Pracawzespole {
-}
